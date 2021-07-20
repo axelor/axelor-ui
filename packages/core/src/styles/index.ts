22608bceb4243b846e3ff3df72bf6601b54a0e3e
Amit Mendapara <amit@axelor.com>
2021-07-20 13:15:51 +0530

    Re-implement styleNames
    Remove classnames dependency.

--- packages/core/src/styles/index.ts
+++ packages/core/src/styles/index.ts
@@ -1,11 +1,27 @@
 import bootstrap from './bootstrap.module.css';
-import classNames, { Argument } from 'classnames';
+
+type Value = string | number | boolean | undefined | null;
+type Argument = Value | Record<string, Value> | Argument[];
+
+const clean = (names: string[]) =>
+  names.flatMap(n => n.trim().split(/\s+/)).filter(Boolean);
+
+const names = (item: Argument): string[] => {
+  if (Array.isArray(item)) return item.flatMap(names);
+  if (typeof item === 'object') {
+    let items: string[] = [];
+    for (let k in item) {
+      if (item[k]) {
+        items.push(k);
+      }
+    }
+    return clean(items);
+  }
+  return item ? clean([item.toString()]) : [];
+};
 
 export function styleNames(...args: Argument[]) {
-  const names = classNames(...args);
-  return names
-    .split(/\s+/g)
-    .map(name => bootstrap[name] ?? name)
-    .filter(style => style)
+  return names(args)
+    .flatMap(name => bootstrap[name] ?? name)
     .join(' ');
 }
