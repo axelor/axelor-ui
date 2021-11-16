f13ea0ee222a48b18fd4ed07655f844772190cba
Amit Mendapara <amit@axelor.com>
2021-11-16 16:14:12 +0530

    Migrate Button demos to stories

--- packages/core/src/button/button.stories.tsx
+++ packages/core/src/button/button.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Button } from './button';
 
@@ -7,19 +7,49 @@ export default {
   title: 'Core/Button',
 };
 
-export const Basic = () => <Button variant="primary">Button</Button>;
+export const Basic = () => {
+  const [text, setText] = useState('Click me...');
+  const onClick = () => {
+    setText('Clicked!!!');
+  };
+  return (
+    <div>
+      <Button variant="primary" onClick={onClick}>
+        {text}
+      </Button>{' '}
+      <Button variant="primary" disabled>
+        Disabled
+      </Button>
+    </div>
+  );
+};
+
+export const Sizes = () => {
+  return (
+    <div>
+      <Button variant="primary" size="sm">
+        Small
+      </Button>{' '}
+      <Button variant="primary">Normal</Button>{' '}
+      <Button variant="primary" size="lg">
+        Large
+      </Button>
+    </div>
+  );
+};
 
 export const Variants = () => {
   return (
     <div>
-      <Button me={1} variant="primary">
-        Primary
-      </Button>
-      <Button me={1} variant="secondary">
-        Secondary
-      </Button>
-      <Button me={1} variant="success">
-        Success
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="info">Info</Button> <Button variant="dark">Dark</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="link" as="a" href="#">
+        Link
       </Button>
     </div>
   );
