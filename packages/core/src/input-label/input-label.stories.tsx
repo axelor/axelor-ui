a1865fbfba808cb59cd3fdb83ed9069d95175720
Amit Mendapara <amit@axelor.com>
2021-11-17 13:28:49 +0530

    Migrate InputLabel demos to stories

---
+++ packages/core/src/input-label/input-label.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { Box } from '../box';
+import { Input } from '../input';
+import { InputLabel } from './input-label';
+
+export default {
+  component: InputLabel,
+  title: 'Core/InputLabel',
+};
+
+export const Basic = () => {
+  return (
+    <Box>
+      <InputLabel>Email</InputLabel>
+      <Input type="email" />
+    </Box>
+  );
+};
+
+export const Disabled = () => {
+  return (
+    <Box>
+      <InputLabel disabled>Email</InputLabel>
+      <Input type="email" disabled />
+    </Box>
+  );
+};
+
+export const Invalid = () => {
+  return (
+    <Box>
+      <InputLabel invalid>Email</InputLabel>
+      <Input type="email" invalid />
+    </Box>
+  );
+};
+
+export const Required = () => {
+  return (
+    <Box>
+      <InputLabel required>Email</InputLabel>
+      <Input type="email" required />
+    </Box>
+  );
+};
