4af5fb95c0f2b44cc1ab0ffe9697778885e8c683
Amit Mendapara <amit@axelor.com>
2021-11-17 13:31:27 +0530

    Migrate List demos to stories

---
+++ packages/core/src/list/list.stories.tsx
@@ -0,0 +1,146 @@
+import React from 'react';
+
+import { Box } from '../box';
+import { Icon } from '../icon';
+import { List } from './list';
+import { ListItem } from './list-item';
+
+export default {
+  component: List,
+  title: 'Core/List',
+};
+
+export const Active = () => {
+  return (
+    <List>
+      <ListItem active>An item</ListItem>
+      <ListItem>A second item</ListItem>
+      <ListItem>A third item</ListItem>
+      <ListItem>A fourth item</ListItem>
+      <ListItem>And a fifth one</ListItem>
+    </List>
+  );
+};
+
+export const Basic = () => {
+  return (
+    <List>
+      <ListItem>An item</ListItem>
+      <ListItem>A second item</ListItem>
+      <ListItem>A third item</ListItem>
+      <ListItem>A fourth item</ListItem>
+      <ListItem>And a fifth one</ListItem>
+    </List>
+  );
+};
+
+const content = (
+  <>
+    <Box d="flex" w={100} justifyContent="space-between">
+      <Box as="h5" mb={1}>
+        Lorem ipsum dolor
+      </Box>
+      <Box as="small" color="muted">
+        sit amet
+      </Box>
+    </Box>
+    <Box as="p" mb={1}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a ultrices
+      velit.
+    </Box>
+    <Box as="small" color="muted">
+      Fusce a ultrices velit.
+    </Box>
+  </>
+);
+
+export const CustomContent = () => {
+  return (
+    <List>
+      <ListItem active>
+        <Box d="flex" w={100} justifyContent="space-between">
+          <Box as="h5" mb={1}>
+            Lorem ipsum dolor
+          </Box>
+          <small>sit amet</small>
+        </Box>
+        <Box as="p" mb={1}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a
+          ultrices velit.
+        </Box>
+        <small>Fusce a ultrices velit.</small>
+      </ListItem>
+      <ListItem>{content}</ListItem>
+      <ListItem>{content}</ListItem>
+    </List>
+  );
+};
+
+export const Disabled = () => {
+  return (
+    <List>
+      <ListItem disabled>An item</ListItem>
+      <ListItem>A second item</ListItem>
+      <ListItem>A third item</ListItem>
+      <ListItem>A fourth item</ListItem>
+      <ListItem>And a fifth one</ListItem>
+    </List>
+  );
+};
+
+export const Flush = () => {
+  return (
+    <List flush>
+      <ListItem>An item</ListItem>
+      <ListItem>A second item</ListItem>
+      <ListItem>A third item</ListItem>
+      <ListItem>A fourth item</ListItem>
+      <ListItem>And a fifth one</ListItem>
+    </List>
+  );
+};
+
+export const FolderList = () => {
+  return (
+    <List>
+      {['archive', 'card-image', 'bank'].map(icon => (
+        <ListItem key={icon}>
+          <Box d="flex" alignItems="center">
+            <Box
+              me={2}
+              p={2}
+              bg="secondary"
+              color="white"
+              rounded="circle"
+              d="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Icon use={icon} />
+            </Box>
+            <Box>
+              <Box as="p" mb={0}>
+                Lorem ipsum
+              </Box>
+              <Box as="small" color="muted">
+                et al dolor el
+              </Box>
+            </Box>
+          </Box>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export const Numbered = () => {
+  return (
+    <List numbered>
+      <ListItem>An item</ListItem>
+      <ListItem>A second item</ListItem>
+      <ListItem>A third item</ListItem>
+      <ListItem>A fourth item</ListItem>
+      <ListItem>And a fifth one</ListItem>
+    </List>
+  );
+};
