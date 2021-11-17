02f4bbad9692eddb4fe67da8bcee3605a13d5356
Amit Mendapara <amit@axelor.com>
2021-11-17 13:28:05 +0530

    Migrate InputFeedback demos to stories

--- packages/core/src/input-feedback/input-feedback.stories.tsx
+++ packages/core/src/input-feedback/input-feedback.stories.tsx
@@ -1,10 +1,27 @@
-/**
- * @title Colors
- */
 import React from 'react';
-import { Box, InputFeedback, Input } from '@axelor-ui/core';
 
-export default () => {
+import { Box } from '../box';
+import { Input } from '../input';
+import { InputFeedback } from './input-feedback';
+
+export default {
+  component: InputFeedback,
+  title: 'Core/InputFeedback',
+};
+
+export const Basic = () => {
+  return (
+    <Box>
+      <Input type="password" />
+      <InputFeedback>
+        Your password must be 8-20 characters long, contain letters and numbers,
+        and must not contain spaces, special characters, or emoji.
+      </InputFeedback>
+    </Box>
+  );
+};
+
+export const Colors = () => {
   return (
     <Box>
       <Input type="text" placeholder="Primary" />
@@ -40,6 +57,17 @@ export default () => {
       <Input type="text" mt={2} placeholder="Dark" />
       <InputFeedback color="dark">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </InputFeedback>
+    </Box>
+  );
+};
+
+export const Invalid = () => {
+  return (
+    <Box>
+      <Input type="password" invalid defaultValue="axelor" />
+      <InputFeedback invalid>
+        Your password must contain a special character
       </InputFeedback>
     </Box>
   );
