6bff7311540046ecfb1515c32b32437e4cb314b8
Amit Mendapara <amit@axelor.com>
2021-11-17 12:59:59 +0530

    Migrate Collapse demos to stories

---
+++ packages/core/src/collapse/collapse.stories.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { Box } from '../box';
+import { Button } from '../button';
+import { Collapse } from './collapse';
+
+export default {
+  component: Collapse,
+  title: 'Core/Collapse',
+};
+
+export const Basic = function () {
+  const [show, setShow] = React.useState(false);
+  const toggle = function () {
+    setShow(prev => !prev);
+  };
+
+  return (
+    <Box>
+      <Button variant="primary" onClick={toggle}>
+        Toggle
+      </Button>
+      <Collapse in={show}>
+        <Box
+          mt={2}
+          rounded
+          bg="secondary"
+          style={{ width: 300, height: 100 }}
+        ></Box>
+      </Collapse>
+    </Box>
+  );
+};
+
+export const Horizontal = function () {
+  const [show, setShow] = React.useState(false);
+  const toggle = function () {
+    setShow(prev => !prev);
+  };
+
+  return (
+    <Box>
+      <Button variant="primary" onClick={toggle}>
+        Toggle
+      </Button>
+      <Collapse in={show} horizontal>
+        <Box
+          mt={2}
+          rounded
+          bg="secondary"
+          style={{ width: 300, height: 100 }}
+        ></Box>
+      </Collapse>
+    </Box>
+  );
+};
