7d1bd88facb382c1e4bdd905339aea0b1b1bca80
Amit Mendapara <amit@axelor.com>
2021-11-17 13:02:40 +0530

    Migrate Divider demos to stories

---
+++ packages/core/src/divider/divider.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Box } from '../box';
+import { Divider } from './divider';
+
+export default {
+  component: Divider,
+  title: 'Core/Divider',
+};
+
+export const Basic = () => {
+  return (
+    <Box p={5}>
+      <Box border p={4} bgColor="light" />
+      <Divider />
+      <Box border p={4} bgColor="light" />
+    </Box>
+  );
+};
+
+export const Vertical = () => {
+  return (
+    <div>
+      <Box d={'flex'} p={5} style={{ height: 200 }}>
+        <Box border p={4} bgColor="light" />
+        <Divider vertical />
+        <Box border p={4} bgColor="light" />
+      </Box>
+    </div>
+  );
+};
