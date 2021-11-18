d219d63357f12b04026f5fac98d40335e90fb4af
Amit Mendapara <amit@axelor.com>
2021-11-18 15:34:29 +0530

    Fix Divider component

--- packages/core/src/divider/divider.tsx
+++ packages/core/src/divider/divider.tsx
@@ -1,33 +1,31 @@
 import React from 'react';
-
+import { styleNames } from '../styles';
 import {
-  AsComponent,
-  AsProp,
-  SystemProps,
+  forwardRef,
   makeStyles,
   omitStyles,
+  OverridableProps,
+  SystemProps
 } from '../system';
-import { styleNames } from '../styles';
 import cssStyles from './divider.module.css';
 
-export interface DividerProps extends SystemProps, AsProp {
+export interface DividerProps extends SystemProps, OverridableProps {
   vertical?: boolean;
 }
 
-export const Divider: AsComponent<'div', DividerProps> = React.forwardRef<
-  HTMLDivElement,
-  DividerProps
->(({ as, className, vertical, ...props }, ref) => {
-  const classes = makeStyles(props);
-  const rest = omitStyles(props);
-  const Component = as || 'hr';
-  return (
-    <Component
-      ref={ref}
-      className={styleNames(cssStyles.divider, className, classes, {
-        [cssStyles.vertical]: vertical,
-      })}
-      {...rest}
-    ></Component>
-  );
-});
+export const Divider = forwardRef<'div', DividerProps>(
+  ({ as, className, vertical, ...props }, ref) => {
+    const classes = makeStyles(props);
+    const rest = omitStyles(props);
+    const Component = as || 'hr';
+    return (
+      <Component
+        ref={ref}
+        className={styleNames(cssStyles.divider, className, classes, {
+          [cssStyles.vertical]: vertical,
+        })}
+        {...rest}
+      ></Component>
+    );
+  }
+);
