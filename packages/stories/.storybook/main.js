678a7b8b3dda17f05692de39b5e636d06e9772a8
Amit Mendapara <amit@axelor.com>
2021-11-19 18:28:35 +0530

    Remove vite and use default storybook builder
    The vite storybook builder is not working properly.

--- packages/stories/.storybook/main.js
+++ packages/stories/.storybook/main.js
@@ -1,27 +1,7 @@
-const svgr = require('vite-plugin-svgr');
-
 module.exports = {
   stories: ['../../**/*.stories.tsx'],
   addons: ['@storybook/addon-a11y', '@storybook/addon-toolbars'],
-  core: {
-    builder: 'storybook-builder-vite',
-  },
   typescript: {
     reactDocgen: false,
-  },
-  async viteFinal(config) {
-    return {
-      ...config,
-      plugins: [...config.plugins, svgr()],
-      resolve: {
-        ...config.resolve,
-        alias: [
-          {
-            find: /^~.+/,
-            replacement: value => value.replace(/^~/, ''),
-          },
-        ],
-      },
-    };
   },
 };
