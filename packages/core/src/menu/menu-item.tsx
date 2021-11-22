224daaed1fa9cd1444049e030d61cb79d033862d
Nishant Painter <n.painter@axelor.com>
2021-11-22 13:24:59 +0530

    Update menu item props
    - Add support for text, label and icon

--- packages/core/src/menu/menu-item.tsx
+++ packages/core/src/menu/menu-item.tsx
@@ -1,4 +1,7 @@
 import { forwardRef } from 'react';
+  
+import { Box } from '../box';
+import { Icon } from '../icon';
 import { styleNames } from '../styles';
 import {
   makeStyles,
@@ -10,41 +13,79 @@ import {
 import cssStyles from './menu.module.css';
 
 export interface MenuItemProps extends SystemProps, OverridableProps {
+  text?: string;
+  startIcon?: string;
+  endIcon?: string;
+  label?: string;
   disabled?: boolean;
   active?: boolean;
   href?: string;
 }
 
-export const MenuItem: OverridableComponent<'div', MenuItemProps> =
-  forwardRef<HTMLAnchorElement, MenuItemProps>(
-    ({ as, disabled, active, className, ...props }, ref) => {
-      const styles = makeStyles(props);
-      const rest = omitStyles(props);
-      const classes = styleNames(
-        className,
-        cssStyles.menuItem,
-        'dropdown-item',
-        {
-          active,
-          disabled,
-        },
-        styles
-      );
-      const Component = as || 'a';
-      return (
-        <Component
-          ref={ref}
-          {...(disabled
-            ? {
-                'tabindex': -1,
-                'aria-disabled': true,
-              }
-            : {
-                tabIndex: 1,
-              })}
-          className={classes}
-          {...rest}
-        ></Component>
-      );
-    }
-  );
+export const MenuItem: OverridableComponent<'div', MenuItemProps> = forwardRef<
+  HTMLAnchorElement,
+  MenuItemProps
+>(
+  (
+    {
+      as,
+      disabled,
+      active,
+      className,
+      text,
+      startIcon,
+      endIcon,
+      label,
+      children,
+      ...props
+    },
+    ref
+  ) => {
+    const styles = makeStyles(props);
+    const rest = omitStyles(props);
+
+    const classes = styleNames(
+      className,
+      cssStyles.menuItem,
+      'dropdown-item',
+      {
+        active,
+        disabled,
+      },
+      styles
+    );
+
+    const Component = as || 'a';
+
+    const content = (
+      <Box d="flex" alignItems="center">
+        {startIcon && <Box as={Icon} me={1} use={startIcon} size="sm" />}
+        <Box d="inline-flex" flexGrow={1}>
+          {children || text}
+        </Box>
+        {label && <Box d="inline-flex">{label}</Box>}
+        {endIcon && (
+          <Box as={Icon} float="end" ms={1} use={endIcon} size="sm" />
+        )}
+      </Box>
+    );
+
+    return (
+      <Component
+        ref={ref}
+        {...(disabled
+          ? {
+              tabIndex: -1,
+              'aria-disabled': true,
+            }
+          : {
+              tabIndex: 1,
+            })}
+        className={classes}
+        {...rest}
+      >
+        {content}
+      </Component>
+    );
+  }
+);
