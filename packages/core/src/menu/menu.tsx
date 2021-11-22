1fd38a0f8aa3d3b2949f6051ea7b360c41717a46
Nishant Painter <n.painter@axelor.com>
2021-11-22 13:26:15 +0530

    Add internal button trigger support in menu

--- packages/core/src/menu/menu.tsx
+++ packages/core/src/menu/menu.tsx
@@ -12,20 +12,25 @@ import { ClickAwayListener } from '../click-away-listener';
 import { ArrowNavigation } from '../arrow-navigation';
 import { FocusTrap } from '../focus-trap';
 import cssStyles from './menu.module.css';
+import { Button } from '../button';
 
 export type MenuPlacement = 'top' | 'bottom' | 'start' | 'end';
 export type MenuAlignment = 'top' | 'bottom' | 'start' | 'end';
 
 export interface MenuProps
-  extends Omit<PopperProps, 'shadow' | 'children'>,
+  extends Omit<PopperProps, 'shadow' | 'children' | 'target'>,
     OverridableProps {
+  target?: HTMLElement | null;
   placement?: MenuPlacement;
   alignment?: MenuAlignment;
   flip?: boolean;
   show?: boolean;
   shadow?: boolean | string;
+  text?: string;
+  onShow?: any;
   onHide?: any;
   className?: string;
+  disablePortal?: boolean;
   children?: React.ReactElement | Array<React.ReactElement>;
 }
 
@@ -38,12 +43,15 @@ export const Menu: OverridableComponent<'div', MenuProps> = React.forwardRef<
       as,
       flip,
       show,
+      text,
+      onShow,
       onHide,
       container = document.body,
       target,
       placement = 'bottom',
       alignment = '',
       className,
+      disablePortal,
       ...props
     },
     ref
@@ -53,29 +61,47 @@ export const Menu: OverridableComponent<'div', MenuProps> = React.forwardRef<
     const Component = as || 'div';
     const $placement: any = `${placement}${alignment ? `-${alignment}` : ''}`;
 
+    const [buttonRef, setButtonRef] = React.useState<any>(null);
+
     return (
-      <Popper placement={$placement} target={target} open={show}>
-        <ClickAwayListener onClickAway={onHide}>
-          <FocusTrap>
-            <ArrowNavigation selector="auto-vertical">
-              <Component
-                ref={ref}
-                className={styleNames(
-                  className,
-                  cssStyles.dropdownMenu,
-                  'dropdown-menu',
-                  {
-                    show,
-                    [`dropdown-menu-${placement}`]: placement,
-                  },
-                  classes
-                )}
-                {...rest}
-              ></Component>
-            </ArrowNavigation>
-          </FocusTrap>
-        </ClickAwayListener>
-      </Popper>
+      <>
+        {text && (
+          <Button
+            ref={setButtonRef}
+            onClick={onShow}
+            {...(show ? { variant: 'light' } : {})}
+          >
+            {text}
+          </Button>
+        )}
+        <Popper
+          placement={$placement}
+          target={target || buttonRef}
+          open={show}
+          disablePortal={disablePortal}
+        >
+          <ClickAwayListener onClickAway={onHide}>
+            <FocusTrap>
+              <ArrowNavigation selector="auto-vertical">
+                <Component
+                  ref={ref}
+                  className={styleNames(
+                    className,
+                    cssStyles.dropdownMenu,
+                    'dropdown-menu',
+                    {
+                      show,
+                      [`dropdown-menu-${placement}`]: placement,
+                    },
+                    classes
+                  )}
+                  {...rest}
+                ></Component>
+              </ArrowNavigation>
+            </FocusTrap>
+          </ClickAwayListener>
+        </Popper>
+      </>
     );
   }
 );
