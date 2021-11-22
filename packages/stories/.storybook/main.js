e1eea6df2f982d48b8dfc8729f3ee5c50501e0ae
Amit Mendapara <amit@axelor.com>
2021-11-22 14:24:07 +0530

    Use storybook webpack5 builder

--- packages/stories/.storybook/main.js
+++ packages/stories/.storybook/main.js
@@ -1,6 +1,9 @@
 module.exports = {
   stories: ['../../**/*.stories.tsx'],
   addons: ['@storybook/addon-a11y', '@storybook/addon-toolbars'],
+  core: {
+    builder: 'webpack5'
+  },
   typescript: {
     reactDocgen: false,
   },
