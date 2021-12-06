097f715594c0df5b1983e3f65a47b711d140289d
Mahadevwala Mayank <m.mahadevwala@axelor.com>
2021-12-06 17:39:31 +0530

    Add nested list demo

--- packages/core/src/list/list.stories.tsx
+++ packages/core/src/list/list.stories.tsx
@@ -1,3 +1,6 @@
+import React, { ReactElement, useState } from 'react';
+
+import { Collapse } from '../collapse';
 import { Box } from '../box';
 import { Icon } from '../icon';
 import { List, ListItem } from './list';
@@ -139,5 +142,137 @@ export const Numbered = () => {
       <ListItem>A fourth item</ListItem>
       <ListItem>And a fifth one</ListItem>
     </List>
+  );
+};
+
+function ListItemIcon({ name }: { name: string }) {
+  return (
+    <Box as="span" me={2}>
+      <Icon use={name} />
+    </Box>
+  );
+}
+
+function NestedList({ children, ...props }: any) {
+  const [activeMenuItem, setActiveMenuItem] = useState<string | null>(null);
+
+  function open(item: string) {
+    setActiveMenuItem(item);
+  }
+
+  function close() {
+    setActiveMenuItem(null);
+  }
+
+  return (
+    <List style={{ paddingLeft: 16 }} {...props}>
+      {React.Children.map(children, item => {
+        if (item && (item as ReactElement).type === NestedListItem) {
+          return React.cloneElement(item as ReactElement, {
+            isOpen: activeMenuItem === (item as ReactElement).props.title,
+            onOpen: open,
+            onClose: close,
+          });
+        }
+        return item;
+      })}
+    </List>
+  );
+}
+
+function NestedListItem({
+  children,
+  title,
+  icon,
+  isOpen,
+  onOpen,
+  onClose,
+}: {
+  children: React.ReactNode;
+  title: string;
+  icon?: string;
+  isOpen?: boolean;
+  onOpen?: (title: string) => void;
+  onClose?: () => void;
+}) {
+  const toggle = function () {
+    if (isOpen) {
+      onClose && onClose();
+    } else {
+      onOpen && onOpen(title);
+    }
+  };
+
+  return (
+    <ListItem>
+      <Box
+        d="flex"
+        justifyContent="space-between"
+        style={{ cursor: 'pointer' }}
+        onClick={toggle}
+      >
+        <Box>
+          {icon && <ListItemIcon name={icon} />}
+          {title}
+        </Box>
+        <Icon use={isOpen ? 'chevron-up' : 'chevron-down'} />
+      </Box>
+      <Collapse in={isOpen}>{children}</Collapse>
+    </ListItem>
+  );
+}
+
+export const Nested = () => {
+  return (
+    <NestedList>
+      <ListItem>
+        <ListItemIcon name={'send-fill'} />
+        Sent mail
+      </ListItem>
+      <ListItem>
+        <ListItemIcon name={'mailbox'} />
+        Drafts
+      </ListItem>
+      <NestedListItem title="Inbox" icon={'inbox-fill'}>
+        <NestedList flush>
+          <ListItem>Primary</ListItem>
+          <NestedListItem title="Social">
+            <List flush>
+              <ListItem>Facebook</ListItem>
+              <ListItem>Twitter</ListItem>
+              <ListItem>Linkedin</ListItem>
+            </List>
+          </NestedListItem>
+          <ListItem>Promotions</ListItem>
+          <NestedListItem title="Updates">
+            <List flush>
+              <ListItem>Github</ListItem>
+              <ListItem>Redmine</ListItem>
+            </List>
+          </NestedListItem>
+          <ListItem>Forums</ListItem>
+        </NestedList>
+      </NestedListItem>
+      <NestedListItem title="Outbox" icon={'send-check-fill'}>
+        <NestedList flush>
+          <ListItem>Primary</ListItem>
+          <NestedListItem title="Social">
+            <List flush>
+              <ListItem>Facebook</ListItem>
+              <ListItem>Twitter</ListItem>
+              <ListItem>Linkedin</ListItem>
+            </List>
+          </NestedListItem>
+          <ListItem>Promotions</ListItem>
+          <NestedListItem title="Updates">
+            <List flush>
+              <ListItem>Github</ListItem>
+              <ListItem>Redmine</ListItem>
+            </List>
+          </NestedListItem>
+          <ListItem>Forums</ListItem>
+        </NestedList>
+      </NestedListItem>
+    </NestedList>
   );
 };
