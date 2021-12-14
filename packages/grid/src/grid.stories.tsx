6f1a1a1b67aee2f8cc15a45b8f01b19b5b3fe017
Mahadevwala Mayank <m.mahadevwala@axelor.com>
2021-12-14 16:03:56 +0530

    Migrate grid demos to stories

---
+++ packages/grid/src/grid.stories.tsx
@@ -0,0 +1,32 @@
+import { Grid } from './grid';
+
+import Basic from './demos/basic';
+import Aggregation from './demos/aggregation';
+import Editable from './demos/editable';
+import Grouping from './demos/grouping';
+import Navigation from './demos/navigation';
+import Renderer from './demos/renderer';
+import Reorder from './demos/reorder';
+import Resizing from './demos/resizing';
+import Searching from './demos/searching';
+import Selection from './demos/selection';
+import Sorting from './demos/sorting';
+
+export default {
+  component: Grid,
+  title: 'Components/Grid',
+};
+
+export {
+  Basic,
+  Sorting,
+  Selection,
+  Navigation,
+  Grouping,
+  Aggregation,
+  Renderer,
+  Resizing,
+  Reorder,
+  Searching,
+  Editable,
+};
