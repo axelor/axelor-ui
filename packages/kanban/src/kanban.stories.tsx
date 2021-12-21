7d66abc60f595eb30fd344bfb0dc356ad5b571d0
Nishant Painter <n.painter@axelor.com>
2021-12-21 17:50:07 +0530

    Add kanban and scheduler stories

---
+++ packages/kanban/src/kanban.stories.tsx
@@ -0,0 +1,324 @@
+import faker from 'faker';
+import { useState, useCallback } from 'react';
+import { Box, Input, Button, Icon, Menu, MenuItem } from '@axelor-ui/core';
+
+import Kanban, { KanbanProps } from './kanban';
+import { CardEvent, Column, ColumnEvent } from './types';
+
+export default {
+  component: Kanban,
+  title: 'Advance/Kanban',
+};
+
+const getId = (() => {
+  let id = 0;
+  return () => id++;
+})();
+
+function getTitle() {
+  return faker.lorem.words();
+}
+
+function getRecords(count: number) {
+  return Array.from({ length: count }, () => ({
+    id: getId(),
+    title: getTitle(),
+  }));
+}
+
+function getDefaultColumns() {
+  return ['Todo', 'In-Progress', 'Completed'].map(title => ({
+    id: getId(),
+    title,
+    records: getRecords(5),
+  }));
+}
+
+function reorder<T>(list: T[], startIndex: number, endIndex: number): T[] {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+}
+
+function reorderCards<T extends Column>({
+  columns,
+  sourceColumn,
+  destinationColumn,
+  sourceIndex,
+  destinationIndex,
+}: {
+  columns: T[];
+  sourceColumn: T;
+  destinationColumn: T;
+  sourceIndex: number;
+  destinationIndex: number;
+}): T[] {
+  const getColumnIndex = (columnId: Column['id']) =>
+    columns.findIndex(c => String(c.id) === String(columnId));
+
+  const getRecords = (columnId: Column['id']) => [
+    ...(columns.find(c => String(c.id) === String(columnId))!.records || []),
+  ];
+
+  const current = getRecords(sourceColumn.id);
+  const next = getRecords(destinationColumn.id);
+  const target = current[sourceIndex];
+
+  // moving to same list
+  if (sourceColumn.id === destinationColumn.id) {
+    const reordered = reorder(current, sourceIndex, destinationIndex);
+    const newColumns = columns.map(c => ({ ...c }));
+    newColumns[getColumnIndex(sourceColumn.id)].records = reordered;
+    return newColumns;
+  }
+
+  // moving to different list
+  current.splice(sourceIndex, 1);
+  next.splice(destinationIndex, 0, target);
+  const newColumns = columns.map(c => ({ ...c }));
+  newColumns[getColumnIndex(sourceColumn.id)].records = current;
+  newColumns[getColumnIndex(destinationColumn.id)].records = next;
+  return newColumns;
+}
+
+function KanbanContainer({ columns: columnsProp, readonly }: KanbanProps) {
+  const [columns, setColumns] = useState<Column[]>(
+    columnsProp || getDefaultColumns()
+  );
+
+  const getColumnIndex = useCallback(
+    columnId => columns.findIndex(c => String(c.id) === String(columnId)),
+    [columns]
+  );
+
+  const getRecordIndex = useCallback(
+    (recordId, columnId) =>
+      columns[getColumnIndex(columnId)]?.records?.findIndex(
+        r => String(r.id) === String(recordId)
+      ),
+    [columns, getColumnIndex]
+  );
+
+  const onCardMove = useCallback(
+    ({ column, index, source, record }: CardEvent) => {
+      const updatedColumns = reorderCards({
+        columns,
+        destinationColumn: column,
+        destinationIndex: index,
+        sourceColumn: source,
+        sourceIndex: getRecordIndex(record.id, source!.id) as number,
+      });
+
+      setColumns(updatedColumns);
+    },
+    [columns, getRecordIndex]
+  );
+
+  const onColumnMove = useCallback(
+    ({ column, index }: ColumnEvent) => {
+      const updatedColumns = reorder(columns, getColumnIndex(column.id), index);
+      setColumns(updatedColumns);
+    },
+    [columns, getColumnIndex]
+  );
+
+  return (
+    <Kanban
+      columns={columns}
+      readonly={readonly}
+      onCardMove={onCardMove}
+      onColumnMove={onColumnMove}
+    />
+  );
+}
+
+export const Basic = () => <KanbanContainer />;
+
+export const ColumnEditor = () => (
+  <KanbanContainer
+    columns={[
+      {
+        id: getId(),
+        title: 'Todo',
+        records: getRecords(3),
+        renderer: ({ RecordList, column }) => (
+          <Box p={2} bg="white" border rounded me={2}>
+            <Box as="h5" p={2}>
+              {column.title}
+            </Box>
+            <Box d="flex">
+              <Input />
+              &nbsp;
+              <Button bg="primary" color="white">
+                Add
+              </Button>
+            </Box>
+            <RecordList column={column} />
+          </Box>
+        ),
+      },
+    ]}
+  />
+);
+
+export const ColumnRenderer = () => (
+  <KanbanContainer
+    columns={[
+      {
+        id: getId(),
+        title: 'Todo',
+        records: getRecords(3),
+        renderer: ({ RecordList, column }) => (
+          <Box p={2} bg="primary" border rounded me={2}>
+            <Box as="h5" p={2} color="white">
+              {column.title}
+            </Box>
+            <RecordList column={column} />
+          </Box>
+        ),
+      },
+    ]}
+  />
+);
+
+export const ColumnScrollable = () => (
+  <KanbanContainer
+    columns={[
+      {
+        id: getId(),
+        title: 'Todo',
+        records: getRecords(10),
+        renderer: ({ RecordList, column }) => (
+          <Box border rounded me={2} p={2} bg="light" style={{ width: 250 }}>
+            <Box as="h5" p={2}>
+              {column.title}
+            </Box>
+            <RecordList
+              column={column}
+              style={{ maxHeight: 250, overflow: 'auto' }}
+            />
+          </Box>
+        ),
+      },
+    ]}
+  />
+);
+
+export const ReadonlyColumn = () => (
+  <KanbanContainer
+    columns={[
+      {
+        id: getId(),
+        title: 'Todo',
+        records: getRecords(3),
+      },
+      {
+        id: getId(),
+        title: 'In-progress',
+        records: getRecords(3),
+      },
+      {
+        id: getId(),
+        title: 'Completed (readonly)',
+        readonly: true,
+        records: getRecords(3),
+      },
+    ]}
+  />
+);
+
+export const Readonly = () => <KanbanContainer readonly />;
+
+const Record = ({ record, column, onEdit, onDelete }: any) => {
+  const [open, setOpen] = useState(false);
+  const [targetEl, setTargetEl] = useState<HTMLButtonElement | null>(null);
+
+  const toggle = () => setOpen(v => !v);
+
+  const closeMenu = () => setOpen(false);
+
+  const handleEdit = () => {
+    onEdit && onEdit({ column, record });
+    setOpen(false);
+  };
+  const handleDelete = () => {
+    onDelete && onDelete({ column, record });
+    setOpen(false);
+  };
+
+  return (
+    <Box p={2} mt={2} shadow="sm" rounded bg="white">
+      <Box pb={1} pt={1} d="flex" justifyContent="space-between">
+        <Box as="p">{record.title}</Box>
+        <Box>
+          <Button p={0} ref={setTargetEl} onClick={toggle} d="inline-flex">
+            <Icon use="caret-down-fill" />
+          </Button>
+        </Box>
+        <Menu show={open} target={targetEl} onHide={closeMenu}>
+          <MenuItem onClick={handleEdit}>Edit</MenuItem>
+          <MenuItem onClick={handleDelete}>Delete</MenuItem>
+        </Menu>
+      </Box>
+    </Box>
+  );
+};
+
+export const RecordMenu = () => {
+  const onEdit = useCallback(() => alert('edit'), []);
+  const onDelete = useCallback(() => alert('delete'), []);
+
+  return (
+    <KanbanContainer
+      columns={[
+        {
+          id: getId(),
+          title: 'Todo',
+          records: [
+            {
+              id: getId(),
+              title: getTitle(),
+              renderer: props => (
+                <Record {...props} {...{ onEdit, onDelete }} />
+              ),
+            },
+          ],
+        },
+      ]}
+    />
+  );
+};
+
+export const RecordRenderer = () => (
+  <KanbanContainer
+    columns={[
+      {
+        id: getId(),
+        title: 'Todo',
+        records: [
+          {
+            id: getId(),
+            title: getTitle(),
+            renderer: ({ record, column }) => {
+              return (
+                <Box
+                  p={2}
+                  mt={2}
+                  shadow="sm"
+                  rounded
+                  bg="primary"
+                  color="white"
+                >
+                  <Box as="p" pb={1} pt={1}>
+                    {record.title}
+                  </Box>
+                </Box>
+              );
+            },
+          },
+        ],
+      },
+    ]}
+  />
+);
---
+++ packages/scheduler/src/scheduler.stories.tsx
@@ -0,0 +1,115 @@
+import moment from 'moment';
+import { useState } from 'react';
+
+import { SchedulerEvent } from './types';
+import Scheduler, { SchedulerProps } from './scheduler';
+
+export default {
+  component: Scheduler,
+  title: 'Advance/Scheduler',
+};
+
+let id = 0;
+
+const now = moment().startOf('day').toDate();
+
+const _events = [
+  {
+    id: 1,
+    title: 'Point in Time Event',
+    start: now,
+    end: now,
+  },
+  {
+    id: 2,
+    title: 'Record dance',
+    start: now,
+    end: now,
+  },
+  {
+    id: 3,
+    title: 'Edit Music',
+    start: now,
+    end: now,
+  },
+];
+
+function SchedulerContainer(props: SchedulerProps) {
+  const [events, setEvents] = useState<any[]>(_events);
+
+  const handleEventCreate: SchedulerProps['onEventCreate'] = ({
+    start,
+    end,
+  }) => {
+    const title = window.prompt('New Event Name');
+
+    if (title) {
+      setEvents(events => [
+        ...events,
+        { id: id++, start, end, title } as SchedulerEvent,
+      ]);
+    }
+  };
+
+  const handleEventDrop: SchedulerProps['onEventDrop'] = ({
+    event,
+    start,
+    end,
+    allDay: _allDay,
+  }) => {
+    let { allDay } = event;
+
+    if (!event.allDay && _allDay) {
+      allDay = true;
+    } else if (event.allDay && !_allDay) {
+      allDay = false;
+    }
+
+    setEvents(events =>
+      events.map(existingEvent =>
+        existingEvent.id == event.id
+          ? ({ ...existingEvent, start, end, allDay } as SchedulerEvent)
+          : existingEvent
+      )
+    );
+  };
+
+  const handleResizeEvent: SchedulerProps['onEventResize'] = ({
+    event,
+    start,
+    end,
+  }) => {
+    setEvents(events =>
+      events.map(existingEvent =>
+        existingEvent.id == event.id
+          ? ({ ...existingEvent, start, end } as SchedulerEvent)
+          : existingEvent
+      )
+    );
+  };
+
+  return (
+    <Scheduler
+      events={events}
+      style={{ height: 500 }}
+      onEventCreate={handleEventCreate}
+      onEventDrop={handleEventDrop}
+      onEventResize={handleResizeEvent}
+      {...props}
+    />
+  );
+}
+
+export const Basic = () => <SchedulerContainer />;
+
+export const Component = () => (
+  <SchedulerContainer
+    components={{
+      event: ({ event }: { event: SchedulerEvent }) => (
+        <span style={{ fontStyle: 'italic' }}>
+          <strong>{event.title}</strong>
+        </span>
+      ),
+    }}
+  />
+);
