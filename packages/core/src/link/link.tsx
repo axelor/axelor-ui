3a25a29fa7087fb4213dfe43b7feff429e136bbc
Amit Mendapara <amit@axelor.com>
2022-01-06 13:50:13 +0530

    Migrate link

--- packages/core/src/link/link.tsx
+++ packages/core/src/link/link.tsx
@@ -1,30 +1,10 @@
-import { forwardRef } from 'react';
+import styled from '../styled';
 
-import { Box } from '../box';
-import { styleNames } from '../styles';
-import { TVariant, SystemProps, makeStyles, omitStyles } from '../system';
-
-export interface LinkProps extends SystemProps {
-  href: string;
-  color?: TVariant;
+export interface LinkProps {
   underline?: boolean;
 }
 
-export const Link = forwardRef<HTMLAnchorElement, LinkProps>(
-  ({ color = 'primary', underline, className, ...props }, ref) => {
-    props.textDecoration = underline ? 'underline' : 'none';
-
-    const styles = makeStyles(props);
-    const rest = omitStyles(props);
-
-    return (
-      <Box
-        as="a"
-        ref={ref}
-        linkColor={color}
-        className={styleNames(styles, className)}
-        {...rest}
-      />
-    );
-  }
-);
+export const Link = styled.a<LinkProps>(({ color = 'primary', underline }) => ({
+  linkColor: color,
+  textDecoration: underline ? 'underline' : 'none',
+}));
