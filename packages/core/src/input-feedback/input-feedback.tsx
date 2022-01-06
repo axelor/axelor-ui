1bac973e8c5379e4865235db1356a3edcb5e646d
Amit Mendapara <amit@axelor.com>
2022-01-06 13:49:41 +0530

    Migrate input-feedback

--- packages/core/src/input-feedback/input-feedback.tsx
+++ packages/core/src/input-feedback/input-feedback.tsx
@@ -1,41 +1,12 @@
-import { forwardRef } from 'react';
+import styled from '../styled';
+import { TVariant } from '../system';
 
-import { styleNames } from '../styles';
-import {
-  OverridableProps,
-  OverridableComponent,
-  makeStyles,
-  omitStyles,
-  SystemProps,
-  TVariant,
-} from '../system';
-
-export interface InputFeedbackProps extends SystemProps, OverridableProps {
+export interface InputFeedbackProps {
   invalid?: boolean;
   color?: TVariant;
 }
 
-export const InputFeedback: OverridableComponent<'div', InputFeedbackProps> =
-  forwardRef<HTMLLabelElement, InputFeedbackProps>(
-    ({ invalid, className, children, as, ...props }, ref) => {
-      const styles = makeStyles(props);
-      const rest = omitStyles(props);
-
-      const classes = styleNames(
-        styles,
-        'form-text',
-        {
-          'text-danger': invalid,
-        },
-        className
-      );
-
-      const Component = as || 'div';
-
-      return (
-        <Component ref={ref} className={classes} {...rest}>
-          {children}
-        </Component>
-      );
-    }
-  );
+export const InputFeedback = styled.div<InputFeedbackProps>(({ invalid }) => [
+  'form-text',
+  { 'text-danger': invalid },
+]);
