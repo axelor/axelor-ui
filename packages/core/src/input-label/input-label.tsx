45d727117f4c0f2c488449f821e8d4acd9e4a90b
Amit Mendapara <amit@axelor.com>
2022-01-06 13:49:33 +0530

    Migrate input-label

--- packages/core/src/input-label/input-label.tsx
+++ packages/core/src/input-label/input-label.tsx
@@ -1,37 +1,19 @@
-import { forwardRef } from 'react';
-
-import { styleNames } from '../styles';
-import { makeStyles, omitStyles, SystemProps } from '../system';
+import styled from '../styled';
 import styles from './input-label.module.css';
 
-export interface InputLabelProps extends SystemProps {
-  htmlFor?: string;
+export interface InputLabelProps {
   required?: boolean;
-  readonly?: boolean;
   disabled?: boolean;
   invalid?: boolean;
 }
 
-export const InputLabel = forwardRef<HTMLLabelElement, InputLabelProps>(
-  ({ invalid, disabled, required, className, children, ...props }, ref) => {
-    const $styles = makeStyles(props);
-    const rest = omitStyles(props);
-
-    const classes = styleNames(
-      $styles,
-      'form-label',
-      {
-        'text-danger': invalid,
-        'text-muted': disabled,
-        [styles.required]: required,
-      },
-      className
-    );
-
-    return (
-      <label ref={ref} className={classes} {...rest}>
-        {children}
-      </label>
-    );
-  }
+export const InputLabel = styled.label<InputLabelProps>(
+  ({ invalid, disabled, required }) => [
+    'form-label',
+    {
+      'text-danger': invalid,
+      'text-muted': disabled,
+      [styles.required]: required,
+    },
+  ]
 );
