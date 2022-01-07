3cbc55ab7ed0a0d08fbcce4f932c7365705edc55
Amit Mendapara <amit@axelor.com>
2022-01-07 17:13:10 +0530

    Migrate stack

--- packages/core/src/stack/stack.tsx
+++ packages/core/src/stack/stack.tsx
@@ -1,18 +1,19 @@
-import { Box, BoxProps } from '../box/box';
-import { forwardRef } from '../system';
-import { styleNames } from '../styles';
+import styled from '../styled';
 
-export interface StackProps extends Omit<BoxProps, 'gap'> {
+export interface StackProps {
   gap?: 0 | 1 | 2 | 3 | 4 | 5;
   horizontal?: boolean;
 }
 
-export const Stack = forwardRef<'div', StackProps>(
-  ({ gap = 0, horizontal = false, className, ...props }, ref) => {
-    const cls = styleNames(className, `gap-${gap}`, {
+export const Stack = styled.div<StackProps>(
+  ({ gap = 0, horizontal }) => [
+    `gap-${gap}`,
+    {
       vstack: !horizontal,
       hstack: !!horizontal,
-    });
-    return <Box display="flex" className={cls} ref={ref} {...props} />;
-  }
+    },
+  ],
+  () => ({
+    display: 'flex',
+  })
 );
