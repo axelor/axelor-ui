68b45b3fb513e0795e5bb0641368b4b1c6fab75b
Amit Mendapara <amit@axelor.com>
2022-02-07 14:37:18 +0530

    Add build script

---
+++ scripts/build.js
@@ -0,0 +1,77 @@
+const glob = require('fast-glob');
+const fse = require('fs-extra');
+const path = require('path');
+const { program } = require('commander');
+
+const ts = require('typescript');
+
+async function doBuild(dir, out) {
+  const configFile = ts.findConfigFile(dir, ts.sys.fileExists, 'tsconfig.json');
+  const { config } = ts.readConfigFile(configFile, ts.sys.readFile);
+
+  config.include = ['src', '*.d.ts'];
+  config.exclude = [
+    'node_modules',
+    'dist',
+    'lib',
+    '**/*.stories.*',
+    '**/stories',
+    '**/demos',
+  ];
+
+  const { options, fileNames, errors } = ts.parseJsonConfigFileContent(
+    config,
+    ts.sys,
+    dir
+  );
+
+  options.outDir = out;
+  options.noEmit = false;
+  options.declaration = true;
+  options.sourceMap = true;
+
+  const tsProgram = ts.createProgram(fileNames, options);
+
+  const { diagnostics, emitSkipped } = tsProgram.emit();
+
+  const allDiagnostics = ts
+    .getPreEmitDiagnostics(tsProgram)
+    .concat(diagnostics, errors);
+
+  if (allDiagnostics.length) {
+    const formatHost = {
+      getCanonicalFileName: path => path,
+      getCurrentDirectory: ts.sys.getCurrentDirectory,
+      getNewLine: () => ts.sys.newLine,
+    };
+    const message = ts.formatDiagnostics(allDiagnostics, formatHost);
+    console.warn(message);
+  }
+
+  if (emitSkipped) {
+    process.exit(1);
+  }
+}
+
+async function doCopy(dir, out) {
+  const src = path.join(dir, 'src');
+  const pattern = ['**/*'];
+  const ignore = ['**/*.{ts,tsx}', '**/demos', '**/stories'];
+  const files = await glob(pattern, { cwd: src, ignore });
+
+  files.forEach(
+    async x => await fse.copy(path.join(src, x), path.join(out, x))
+  );
+}
+
+async function action(dir, { outDir = 'dist' } = {}) {
+  await doBuild(dir, outDir);
+  await doCopy(dir, outDir);
+}
+
+program
+  .argument('<package>', 'package directory')
+  .option('-o, --out-dir <dir>', 'output directory')
+  .action(action);
+
+program.parse(process.argv);
