701f28ac168021667f4b9ce33d6d43c276d8eef3
Nishant Painter <n.painter@axelor.com>
2022-02-10 16:07:59 +0530

    Add build scripts

--- packages/grid/declare.d.ts
+++ packages/grid/declare.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="vite/client" />
-/// <reference types="vite-plugin-svgr/client" />
-
+declare module '*.module.scss';
 declare module '*.module.css';
+declare module '*.svg';
--- packages/scheduler/declare.d.ts
+++ packages/scheduler/declare.d.ts
@@ -1,1 +1,3 @@
+declare module '*.module.scss';
 declare module '*.module.css';
+declare module '*.svg';
