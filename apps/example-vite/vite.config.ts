2d4169d5f529c5c29ad4694951ebe4c90fd451e2
Amit Mendapara <amit@axelor.com>
2022-02-11 18:47:51 +0530

    Add Vite example

---
+++ apps/example-vite/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), svgrPlugin({ svgrOptions: { icon: true } })],
+  optimizeDeps: {
+    include: ['react/jsx-runtime'],
+  },
+  resolve: {
+    alias: [
+      {
+        find: /^~(.*)/,
+        replacement: '$1',
+      },
+    ],
+  },
+});
