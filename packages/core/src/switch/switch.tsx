fe28f77ab7a954f092dddea3f1a80973d4832e9c
Mahadevwala Mayank <m.mahadevwala@axelor.com>
2022-04-14 17:59:36 +0530

    Fix useClassnames in switch component

--- packages/core/src/switch/switch.tsx
+++ packages/core/src/switch/switch.tsx
@@ -1,6 +1,6 @@
 import { Input } from '../input';
 import styled, { withStyled } from '../styled';
-import { useStyleNames } from '../system';
+import { useClassNames } from '../styles';
 import styles from './switch.module.css';
 
 export interface SwitchProps {
@@ -18,9 +18,9 @@ const SwitchInput = styled(Input)<SwitchProps>(
 );
 
 export const Switch = withStyled(SwitchInput)((props, ref) => {
-  const className = useStyleNames(() => ['form-check', 'form-switch'], []);
+  const classNames = useClassNames();
   return (
-    <div className={className}>
+    <div className={classNames(['form-check', 'form-switch'])}>
       <SwitchInput {...props} ref={ref} />
     </div>
   );
