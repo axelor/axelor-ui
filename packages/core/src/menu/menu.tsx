a99c7936a5206ffb23fb76d3ac5d7e1ca0540818
Nishant Painter <n.painter@axelor.com>
2022-04-25 15:32:00 +0530

    Update menu
    - Remove button trigger support

--- packages/core/src/menu/menu.tsx
+++ packages/core/src/menu/menu.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
 import { ArrowNavigation } from '../arrow-navigation';
-import { Button } from '../button';
+
 import { ClickAwayListener } from '../click-away-listener';
 import { FocusTrap } from '../focus-trap';
 import { Popper, PopperProps } from '../popper/popper';
@@ -15,8 +14,6 @@ export interface MenuProps
   target?: HTMLElement | null;
   flip?: boolean;
   show?: boolean;
-  text?: string;
-  onShow?: any;
   onHide?: any;
   disablePortal?: boolean;
   navigation?: boolean;
@@ -31,17 +28,11 @@ const MenuContent = styled.div<MenuProps>(({ show, placement }) => [
   },
 ]);
 
-const MenuButton = styled(Button)<MenuProps>(props => {
-  return props.show ? { variant: 'light' } : {};
-});
-
 export const Menu = withStyled(MenuContent)(
   (
     {
       flip,
       show,
-      text,
-      onShow,
       onHide,
       container = document.body,
       target,
@@ -55,46 +46,38 @@ export const Menu = withStyled(MenuContent)(
     },
     ref
   ) => {
-    const [buttonRef, setButtonRef] = useState<HTMLButtonElement | null>(null);
     return (
-      <>
-        {text && (
-          <MenuButton ref={setButtonRef} onClick={onShow} show={show}>
-            {text}
-          </MenuButton>
-        )}
-        <Popper
-          placement={placement}
-          target={target || buttonRef}
-          open={show}
-          offset={offset}
-          disablePortal={disablePortal}
-          arrow={arrow}
-          transition={transition}
-        >
-          <ClickAwayListener onClickAway={onHide}>
-            {navigation ? (
-              <FocusTrap>
-                <ArrowNavigation selector="auto-vertical">
-                  <MenuContent
-                    {...props}
-                    show={show}
-                    placement={placement}
-                    ref={ref}
-                  />
-                </ArrowNavigation>
-              </FocusTrap>
-            ) : (
-              <MenuContent
-                {...props}
-                show={show}
-                placement={placement}
-                ref={ref}
-              />
-            )}
-          </ClickAwayListener>
-        </Popper>
-      </>
+      <Popper
+        placement={placement}
+        target={target!}
+        open={show}
+        offset={offset}
+        disablePortal={disablePortal}
+        arrow={arrow}
+        transition={transition}
+      >
+        <ClickAwayListener onClickAway={onHide}>
+          {navigation ? (
+            <FocusTrap>
+              <ArrowNavigation selector="auto-vertical">
+                <MenuContent
+                  {...props}
+                  show={show}
+                  placement={placement}
+                  ref={ref}
+                />
+              </ArrowNavigation>
+            </FocusTrap>
+          ) : (
+            <MenuContent
+              {...props}
+              show={show}
+              placement={placement}
+              ref={ref}
+            />
+          )}
+        </ClickAwayListener>
+      </Popper>
     );
   }
 );
