e9661437b0a84f178e0f2f68e8c8b95779b6d9c8
Nishant Painter <n.painter@axelor.com>
2022-04-27 13:39:08 +0530

    Add global direction toolbar for stories

--- apps/stories/.storybook/preview.js
+++ apps/stories/.storybook/preview.js
@@ -1,3 +1,5 @@
+import { ThemeProvider } from '../../../packages/core/src/styles';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -19,3 +21,31 @@ export const parameters = {
     },
   },
 };
+
+export const globalTypes = {
+  direction: {
+    name: 'Direction',
+    description: 'Direction of document',
+    defaultValue: 'ltr',
+    toolbar: {
+      icon: 'redirect',
+      items: [
+        { value: 'ltr', title: 'LTR' },
+        { value: 'rtl', title: 'RTL' },
+      ],
+    },
+  },
+};
+
+const withThemeProvider = (Story, context) => {
+  const dir = context.globals.direction;
+  return (
+    <ThemeProvider dir={dir}>
+      <div>
+        <Story {...context} />
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export const decorators = [withThemeProvider];
