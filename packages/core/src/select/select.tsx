0dcf0eb3ed9531c7a03104be1c86e341a89b7c18
Mahadevwala Mayank <m.mahadevwala@axelor.com>
2022-05-26 16:44:23 +0530

    Fix select rtl

--- packages/core/src/select/select.tsx
+++ packages/core/src/select/select.tsx
@@ -266,6 +266,7 @@ export function Select({
           IndicatorsContainer,
           MenuList,
           icons,
+          MenuList,
           ...components,
         },
         ...(isCreatable
