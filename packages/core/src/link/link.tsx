f70ec19d202f0e4448a3823b13794a4cefd66ad0
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2022-09-21 19:42:44 +0530

    Add styles for link

--- packages/core/src/link/link.tsx
+++ packages/core/src/link/link.tsx
@@ -1,10 +1,14 @@
 import styled from '../styled';
+import styles from './link.module.scss';
 
 export interface LinkProps {
   underline?: boolean;
 }
 
-export const Link = styled.a<LinkProps>(({ color = 'primary', underline }) => ({
-  linkColor: color,
-  textDecoration: underline ? 'underline' : 'none',
-}));
+export const Link = styled.a<LinkProps>(
+  ({ color = 'primary' }) => [styles[`link-${color}`]],
+  ({ color = 'primary', underline }) => ({
+    linkColor: color,
+    textDecoration: underline ? 'underline' : 'none',
+  })
+);
