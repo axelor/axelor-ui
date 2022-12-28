fbe3140e8e72e5b5c51880d6ed65865799605e33
Amit Mendapara <amit@axelor.com>
2022-12-28 18:51:24 +0530

    Use only minimal storybook addons

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,6 +1,13 @@
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-viewport',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-toolbars',
+    '@storybook/addon-measure',
+    '@storybook/addon-outline',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
