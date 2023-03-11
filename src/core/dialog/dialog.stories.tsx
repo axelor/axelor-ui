247cbcb999aa7edcebbad83eb92a562246f943a0
Amit Mendapara <amit@axelor.com>
2023-03-11 16:46:29 +0100

    Implement Dialog component

---
+++ src/core/dialog/dialog.stories.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import { Box } from '../box';
+import { Button } from '../button';
+import { Input } from '../input';
+import { InputLabel } from '../input-label';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from './dialog';
+
+const config = {
+  component: Dialog,
+  title: 'Components/Dialog',
+};
+
+export const Basic = (props: any) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const [scrollable, setScrollable] = useState<boolean>(false);
+  const [fullScreen, setFullScreen] = useState<boolean>(false);
+  const handleClose = () => setOpen(false);
+  const handleShow = () => setOpen(true);
+  return (
+    <div>
+      <Box g={2} display="flex" alignItems="center">
+        <Button variant="primary" onClick={handleShow}>
+          Show Dialog
+        </Button>
+        <InputLabel m={0}>
+          <Input
+            type="checkbox"
+            checked={scrollable}
+            onChange={e => setScrollable(!scrollable)}
+          />{' '}
+          Scrollable
+        </InputLabel>
+        <InputLabel m={0}>
+          <Input
+            type="checkbox"
+            checked={fullScreen}
+            onChange={e => setFullScreen(!fullScreen)}
+          />{' '}
+          Fullscreen
+        </InputLabel>
+      </Box>
+      <Dialog
+        open={open}
+        backdrop
+        scrollable={scrollable}
+        fullscreen={fullScreen}
+      >
+        <DialogHeader onCloseClick={handleClose}>
+          <DialogTitle>Information</DialogTitle>
+        </DialogHeader>
+        <DialogContent>
+          <p>
+            This is some placeholder content to show the scrolling behavior for
+            modals. Instead of repeating the text the modal, we use an inline
+            style set a minimum height, thereby extending the length of the
+            overall modal and demonstrating the overflow scrolling. When content
+            becomes longer than the height of the viewport, scrolling will move
+            the modal as needed.
+          </p>
+          <p style={{ height: 500 }}></p>
+        </DialogContent>
+        <DialogFooter>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">OK</Button>
+        </DialogFooter>
+      </Dialog>
+    </div>
+  );
+};
+
+export default config;
---
+++ src/core/dialog/dialog.tsx
@@ -0,0 +1,173 @@
+import { useCallback } from 'react';
+import { Box } from '../box';
+import { Fade } from '../fade';
+import { FocusTrap } from '../focus-trap';
+import { Portal } from '../portal';
+import styled, { withStyled } from '../styled';
+import { clsx, useClassNames } from '../styles';
+import styles from './dialog.module.scss';
+
+export interface DialogProps {
+  open: boolean;
+  className?: string;
+  children?: React.ReactNode;
+  backdrop?: boolean;
+  scrollable?: boolean;
+  fullscreen?: boolean;
+  centered?: boolean;
+  size?: 'sm' | 'md' | 'lg' | 'xl';
+  onShow?: () => void;
+  onHide?: () => void;
+}
+
+export interface DialogTitleProps {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export interface DialogHeaderProps {
+  className?: string;
+  children?: React.ReactNode;
+  onCloseClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface DialogFooterProps {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export interface DialogContentProps {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+const TitleRoot = styled.h1<DialogTitleProps>();
+
+export const DialogTitle = withStyled(TitleRoot)((props, ref) => {
+  const { className, children, ...rest } = props;
+  const classNames = useClassNames();
+  return (
+    <TitleRoot
+      fontSize={5}
+      className={clsx(className, classNames('modal-title'))}
+      ref={ref}
+      {...rest}
+    >
+      {children}
+    </TitleRoot>
+  );
+});
+
+const HeaderRoot = styled.div<DialogHeaderProps>();
+
+export const DialogHeader = withStyled(HeaderRoot)((props, ref) => {
+  const { className, children, onCloseClick, ...rest } = props;
+  const classNames = useClassNames();
+  return (
+    <HeaderRoot
+      ref={ref}
+      className={clsx(className, classNames('modal-header'))}
+      {...rest}
+    >
+      <Box>{children}</Box>
+      {onCloseClick && (
+        <Box>
+          <Box
+            as="button"
+            className={classNames('btn-close')}
+            onClick={onCloseClick}
+          />
+        </Box>
+      )}
+    </HeaderRoot>
+  );
+});
+
+const FooterRoot = styled.div<DialogFooterProps>();
+
+export const DialogFooter = withStyled(FooterRoot)((props, ref) => {
+  const { className, children } = props;
+  const classNames = useClassNames();
+  return (
+    <Box
+      ref={ref}
+      className={clsx(className, classNames('modal-footer'))}
+      {...props}
+    >
+      {children}
+    </Box>
+  );
+});
+
+const ContentRoot = styled.div<DialogContentProps>();
+
+export const DialogContent = withStyled(ContentRoot)((props, ref) => {
+  const { className, children } = props;
+  const classNames = useClassNames();
+  return (
+    <ContentRoot
+      ref={ref}
+      className={clsx(className, classNames('modal-body'))}
+      {...props}
+    >
+      {children}
+    </ContentRoot>
+  );
+});
+
+export const Dialog = withStyled(Box)<DialogProps>((props, ref) => {
+  const {
+    open,
+    size,
+    backdrop,
+    centered,
+    scrollable,
+    fullscreen,
+    onShow,
+    onHide,
+    className,
+    children,
+    ...rest
+  } = props;
+  const classNames = useClassNames();
+
+  const onEntered = useCallback(() => {
+    if (onShow) onShow();
+  }, [onShow]);
+
+  const onExited = useCallback(() => {
+    if (onHide) onHide();
+  }, [onHide]);
+
+  return (
+    <Portal>
+      {backdrop && (
+        <Fade in={open}>
+          <Box className={classNames('modal-backdrop', 'show', 'fade')} />
+        </Fade>
+      )}
+      <Fade in={open} onEntered={onEntered} onExited={onExited}>
+        <Box
+          className={clsx(className, styles.dialogRoot, classNames('modal'))}
+          {...rest}
+        >
+          <Box
+            className={classNames('modal-dialog', {
+              'modal-dialog-centered': centered,
+              'modal-dialog-scrollable': scrollable,
+              'modal-fullscreen': fullscreen,
+              'modal-sm': size === 'sm',
+              'modal-lg': size === 'lg',
+              'modal-xl': size === 'xl',
+            })}
+            ref={ref}
+          >
+            <FocusTrap enabled={open}>
+              <Box className={classNames('modal-content')}>{children}</Box>
+            </FocusTrap>
+          </Box>
+        </Box>
+      </Fade>
+    </Portal>
+  );
+});
