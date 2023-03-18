8f006dc8bb953d174f1816d1e717c160274167d0
Amit Mendapara <amit@axelor.com>
2023-03-18 12:18:31 +0100

    Upgrade to typescript 5
    Also upgrade vite and switch back or standard vite react plugin
    as typescript 5 is already quite fast.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
