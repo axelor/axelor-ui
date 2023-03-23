be9685835bd60f4bf65319843f2f79029eaa39cf
Amit Mendapara <amit@axelor.com>
2023-03-23 11:21:10 +0100

    Improve FocusTrap to allow setting initial focus target

--- src/core/focus-trap/focus-trap.tsx
+++ src/core/focus-trap/focus-trap.tsx
@@ -1,14 +1,17 @@
-import * as React from "react";
 import * as focusTrap from "focus-trap";
+import * as React from "react";
 import { useRefs } from "../hooks";
+
+export type FocusTarget = focusTrap.FocusTargetOrFalse;
 
 export interface FocusTrapProps {
   enabled?: boolean;
   children: React.ReactElement;
+  initialFocus?: FocusTarget;
 }
 
 export const FocusTrap = React.forwardRef<HTMLElement, FocusTrapProps>(
-  ({ enabled = true, children }: FocusTrapProps, ref) => {
+  ({ enabled = true, children, initialFocus }: FocusTrapProps, ref) => {
     const childrenRef = React.useRef<HTMLElement | null>(null);
 
     // @ts-expect-error
@@ -22,13 +25,14 @@ export const FocusTrap = React.forwardRef<HTMLElement, FocusTrapProps>(
           allowOutsideClick: true,
           fallbackFocus: children,
           escapeDeactivates: false,
+          initialFocus,
         });
         trap.activate();
         return () => {
           trap.deactivate();
         };
       }
-    }, [enabled]);
+    }, [enabled, initialFocus]);
 
     return React.cloneElement(children, {
       ref: handleRef,
