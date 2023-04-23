876e7956df216fa832aa6df926bee431e5e980e5
Amit Mendapara <amit@axelor.com>
2023-04-23 08:07:42 +0530

    Add optimizeDeps settings

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,35 +1,42 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-import { mergeConfig } from 'vite';
+import type { StorybookConfig } from "@storybook/react-vite";
+import path from "path";
+import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.tsx'],
+  stories: ["../src/**/*.stories.tsx"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: "@storybook/react-vite",
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
   },
   core: {
     disableTelemetry: true,
   },
   viteFinal(config) {
+    const src = path.resolve(__dirname, "../src");
+    const cacheDir = path.resolve(__dirname, "../node_modules/.cache/vite");
     return mergeConfig(config, {
       resolve: {
         alias: [
           {
             find: /^~(.*)/,
-            replacement: '$1',
+            replacement: "$1",
           },
         ],
       },
       define: {
-        'process.env': {},
+        "process.env": {},
+      },
+      cacheDir,
+      optimizeDeps: {
+        entries: [`${src}/**/*.{ts,js,tsx,jsx,mdx,css,scss,html}`],
       },
     });
   },
