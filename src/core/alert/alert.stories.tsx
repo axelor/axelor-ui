f4b4f58e3eae3f25ed7e00b59adecb385570df7c
Amit Mendapara <amit@axelor.com>
2023-06-10 20:16:13 +0200

    Improve alert stories

--- src/core/alert/alert.stories.tsx
+++ src/core/alert/alert.stories.tsx
@@ -1,9 +1,6 @@
+import { MaterialIcon } from "../../icons/meterial-icon";
 import { Box } from "../box";
-import { Icon } from "../icon";
 import { Alert, AlertHeader, AlertLink } from "./alert";
-
-import { ReactComponent as BiExclamation } from "bootstrap-icons/icons/exclamation-circle-fill.svg";
-import { ReactComponent as BiClose } from "bootstrap-icons/icons/x.svg";
 
 const config = {
   component: Alert,
@@ -20,7 +17,7 @@ export const Basic = () => {
       </Alert>
       <Alert variant="danger">
         <Box alignItems="center" d="flex" g={2}>
-          <Icon as={BiExclamation} />
+          <MaterialIcon icon="exclamation" />
           <Box>
             This is an <AlertLink href="/">alert with</AlertLink> icon...
           </Box>
@@ -29,7 +26,7 @@ export const Basic = () => {
       <Alert variant="success">
         <Box alignItems="center" d="flex" g={2}>
           <Box flex={1}>This is an alert with close icon...</Box>
-          <Icon as={BiClose} cursor="pointer" />
+          <MaterialIcon icon="close" />
         </Box>
       </Alert>
     </Box>
