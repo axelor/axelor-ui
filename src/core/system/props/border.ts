234abc0e5cd3158a7214b90b6ec8033ffcc910e1
Amit Mendapara <amit@axelor.com>
2023-06-10 17:38:22 +0200

    Add translucent border support

--- src/core/system/props/border.ts
+++ src/core/system/props/border.ts
@@ -9,6 +9,7 @@ export interface BorderProps {
   borderEnd?: boolean;
   borderWidth?: 0 | 1 | 2 | 3 | 4 | 5;
   borderColor?: TVariant;
+  borderTranslucent?: boolean;
   rounded?: boolean | 0 | 1 | 2 | 3 | "circle" | "pill";
   roundedTop?: boolean;
   roundedBottom?: boolean;
@@ -40,6 +41,15 @@ export const BorderConfig: Config<BorderProps> = {
   borderEnd: border("border-end"),
   borderWidth: border("border"),
   borderColor: border("border"),
+  borderTranslucent: (value) => {
+    if (value) {
+      return {
+        styles: {
+          "--bs-border-color": "var(--bs-border-color-translucent)",
+        } as any,
+      };
+    }
+  },
   rounded: rounded(),
   roundedTop: roundedSide("rounded-top"),
   roundedBottom: roundedSide("rounded-bottom"),
