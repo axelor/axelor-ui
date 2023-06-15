14e82f225ed2232a26dba4721388d5c5bd27fd8b
Amit Mendapara <amit@axelor.com>
2023-06-15 18:31:51 +0200

    Remove tiled resolution support
    It was done to support CRA but it's out dated now.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgrPlugin({ svgrOptions: { icon: true } })],
-  resolve: {
-    alias: [
-      {
-        find: /^~(.*)/,
-        replacement: "$1",
-      },
-    ],
-  },
 });
