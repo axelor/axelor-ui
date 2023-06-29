5526ff05b557d731f0272a05a37605df8507af8d
Amit Mendapara <amit@axelor.com>
2023-06-29 15:13:01 +0200

    Harmonize BootstrapIcon with MaterialIcon

--- src/icons/bootstrap-icon/bootstrap-icon.tsx
+++ src/icons/bootstrap-icon/bootstrap-icon.tsx
@@ -2,21 +2,36 @@ import styles from "./bootstrap-icon.module.scss";
 
 import iconNames from "bootstrap-icons/font/bootstrap-icons.json";
 import { forwardRef } from "react";
+import { Box, TForeground } from "../../core";
 import { clsx } from "../../core/clsx";
 
 export type BootstrapIconName = keyof typeof iconNames;
 
 export interface BootstrapIconProps {
   icon: BootstrapIconName;
+  fill?: boolean;
+  fontSize?: number | string;
+  color?: TForeground;
   className?: string;
   onClick?: React.MouseEventHandler<HTMLElement>;
 }
 
 export const BootstrapIcon = forwardRef<HTMLElement, BootstrapIconProps>(
   (props, ref) => {
-    const { className, onClick, icon } = props;
-    const cls = `bi-${icon}`;
+    const { className, onClick, icon, fill, color, fontSize } = props;
+    const name = fill && icon.endsWith("-fill") ? icon : `${icon}-fill`;
+    const cls = `bi-${name}`;
     const clsName = clsx(className, styles["bi"], styles[cls]);
-    return <i className={clsName} onClick={onClick} ref={ref} />;
+
+    return (
+      <Box
+        as="span"
+        className={clsName}
+        color={color}
+        style={{ fontSize }}
+        onClick={onClick}
+        ref={ref}
+      />
+    );
   }
 );
