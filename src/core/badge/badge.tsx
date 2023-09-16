91a52bb53fbe6f4bd0080224f04980eca3eb822a
Amit Mendapara <amit@axelor.com>
2023-09-16 15:59:31 +0200

    Improve Badge component
    Add `variant` property.

--- src/core/badge/badge.tsx
+++ src/core/badge/badge.tsx
@@ -1,4 +1,18 @@
 import styled from "../styled";
+import { TVariant } from "../system";
+
 import styles from "./badge.module.scss";
 
-export const Badge = styled.span(() => ["badge", styles.badge]);
+export interface BadgeProps {
+  variant?: TVariant;
+}
+
+export const Badge = styled.span<BadgeProps>(
+  () => [styles.badge],
+  ({ variant }) => [
+    {
+      [`badge`]: true,
+      [`text-bg-${variant}`]: variant,
+    },
+  ],
+);
