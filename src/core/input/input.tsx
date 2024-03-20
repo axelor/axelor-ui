8beff1286f0aba97be27f64285084ab0166a596c
Phi-Long Do <p.do@axelor.com>
2024-03-20 14:57:47 +0100

    Implement AdornedInput component
    AdornedInput is a component that wraps an input and optional adornments.
    
    Only endAdornment for now.

--- src/core/input/input.tsx
+++ src/core/input/input.tsx
@@ -1,4 +1,7 @@
+import { ComponentProps, forwardRef } from "react";
+import { useClassNames } from "..";
 import styled from "../styled";
+
 import styles from "./input.module.scss";
 
 export interface InputProps {
@@ -22,17 +25,70 @@ const formClass = (type: string) => {
   }
 };
 
+const inputClassNames = (
+  invalid?: boolean,
+  large?: boolean,
+  small?: boolean,
+) => ({
+  [styles.invalid]: invalid,
+  [styles["input-sm"]]: small,
+  [styles["input-lg"]]: large,
+  "form-control-sm": small,
+  "form-control-lg": large,
+});
+
 export const Input = styled.input<InputProps>(
   ({ type = "text", invalid, large, small }) => [
     styles.input,
     formClass(type),
-    {
-      [styles.invalid]: invalid,
-      [styles["input-sm"]]: small,
-      [styles["input-lg"]]: large,
-      "form-control-sm": small,
-      "form-control-lg": large,
-    },
+    inputClassNames(invalid, large, small),
   ],
   ({ type = "text" }) => ({ type }),
 );
+
+export const AdornedInput = forwardRef<
+  HTMLInputElement,
+  ComponentProps<typeof Input> & {
+    endAdornment?: JSX.Element;
+  }
+>((props, ref) => {
+  const {
+    endAdornment,
+    type = "text",
+    invalid,
+    large,
+    small,
+    className,
+    ...inputProps
+  } = props;
+  const classNames = useClassNames();
+
+  if (!endAdornment) {
+    return (
+      <Input
+        ref={ref}
+        type={type}
+        invalid={invalid}
+        large={large}
+        small={small}
+        className={className}
+        {...inputProps}
+      />
+    );
+  }
+
+  return (
+    <span
+      className={classNames(
+        styles.adorned,
+        styles.input,
+        formClass(type),
+        inputClassNames(invalid, large, small),
+        className,
+      )}
+    >
+      <Input ref={ref} type={type} {...inputProps} />
+      <span className={styles.endAdornment}>{endAdornment}</span>
+    </span>
+  );
+});
